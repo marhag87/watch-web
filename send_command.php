de699c6f06f0f079a4f60f8796b85cfc0a0b2303
Martin Hagstrom <marhag87@gmail.com>
2014-08-24 13:38:02 +0200

    Run commands on local machine

--- send_command.php
+++ send_command.php
@@ -2,7 +2,7 @@
 if (empty($_POST["file"])) {
   echo "Empty filename";
 } else {
-  $command = '/usr/bin/ssh martin@pluto "/usr/local/bin/playontv \'' . $_POST["file"] . '\'"';
+  $command = 'sudo -u martin /usr/local/bin/playontv \'' . $_POST["file"] . '\'';
   exec($command);
   echo "Sent command: " . $command;
 }
